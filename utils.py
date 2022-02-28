4cde1bdd46722f8b30044418b7f1cfd4cd8426af
randomhondadriver <100531858+randomhondadriver@users.noreply.github.com>
2022-02-27 22:17:46 -0500

    add: basic funtionalities for bot

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import functools
-
+import time
 """
 usage:
 def decorator(func):
@@ -8,3 +8,43 @@ def decorator(func):
         return func(*args,**kwargs)
     return wrapper_decorator
 """
+
+
+def timer(func):
+    @functools.wraps(func)
+    def wrapper_decorator(*args,**kwargs):
+        start = time.perf_counter()
+        value = func(*args,**kwargs)
+        end = time.perf_counter()
+        run_time = end-start
+        print(f"{run_time} secs passed...")
+        return value
+    return wrapper_decorator
+
+def silly_intro(func):
+    @functools.wraps(func)
+    def wrapper_decorator(*args,**kwargs):
+        print("Hey! Thank you so much for choosing to support us in "
+            "yet another way. It really means the world to us. "
+            "Every contribution you make, big or small helps us "
+            "along our journey to becoming the best app. ")
+        value = func(*args,**kwargs)
+        return value
+    return wrapper_decorator
+
+def pretend_busy(func):
+    @functools.wraps(func)
+    def wrapper_decorator(*args,**kwargs):
+        time.sleep(1)
+        value = func(*args,**kwargs)
+        return value
+    return wrapper_decorator
+    
+def callin(func):
+    @functools.wraps(func)
+    def wrapper_decorator(*args,**kwargs):
+        print("Brrr..Brrr..........brrr..brrr...........brrr..brrrr...........")
+        value = func(*args,**kwargs)
+        return value
+    return wrapper_decorator
+        
